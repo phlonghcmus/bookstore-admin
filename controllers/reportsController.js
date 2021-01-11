ae2febab2ef488c639c2010a1bdb992a7811f60e
DucLoc <ducloc0009@gmail.com>
2021-01-11 17:52:44 +0700

    + Fix report

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -4,14 +4,18 @@ const ordersModel = require('../models/ordersModel');
 exports.month =async (req, res) => {
     value=[0,0,0,0,0,0,0,0,0,0,0,0,0]
     const orders= await ordersModel.list();
-    console.log(orders);
+   
     var year_system= new Date().getFullYear();
     (orders).forEach(element => {
-        let new_time=moment(element.order_date,"DD/MM/YY - HH:mm");
+        
+        let new_time=moment(element.order_date,"DD/MM/YYYY - HH:mm");
         var date=new_time.toDate();
+    
         month=date.getMonth()+1;
-        year=date.getFullYear()
-        if (year_system==year)
+       
+        year=date.getFullYear();
+        
+        if (year_system===year)
             value[month]=value[month]+element.total;
 
     });
@@ -20,10 +24,10 @@ exports.month =async (req, res) => {
 exports.precious =async (req, res) => {
     value=[0,0,0,0,0]
     const orders= await ordersModel.list();
-    console.log(orders);
+    
     var year_system= new Date().getFullYear();
     (orders).forEach(element => {
-        let new_time=moment(element.order_date,"DD/MM/YY - HH:mm");
+        let new_time=moment(element.order_date,"DD/MM/YYYY - HH:mm");
         var date=new_time.toDate();
         month=date.getMonth()+1;
         year=date.getFullYear()
@@ -44,10 +48,10 @@ exports.precious =async (req, res) => {
 exports.year =async (req, res) => {
     value=[0,0,0,0,0]
     const orders= await ordersModel.list();
-    console.log(orders);
+    
     var year_system= new Date().getFullYear();
     (orders).forEach(element => {
-        let new_time=moment(element.order_date,"DD/MM/YY - HH:mm");
+        let new_time=moment(element.order_date,"DD/MM/YYYY - HH:mm");
         var date=new_time.toDate();
         month=date.getMonth()+1;
         year=date.getFullYear()
@@ -74,30 +78,7 @@ exports.year =async (req, res) => {
     });
     res.render('reports/year',{admin : "Admin,",logout: "Logout",value,year_system});
 };
-exports.precious =async (req, res) => {
-    value=[0,0,0,0,0]
-    const orders= await ordersModel.list();
-    console.log(orders);
-    var year_system= new Date().getFullYear();
-    (orders).forEach(element => {
-        let new_time=moment(element.order_date,"DD/MM/YY - HH:mm");
-        var date=new_time.toDate();
-        month=date.getMonth()+1;
-        year=date.getFullYear()
-        if (year_system==year)
-        {
-            if (month<=3)
-                value[1]=value[1]+element.total;
-            if (month>3 && month<=6)
-                value[2]=value[2]+element.total;
-            if (month>6 && month<=9)
-                value[3]=value[3]+element.total;
-                if (month>9 && month<=12)
-                value[4]=value[4]+element.total;
-        }
-    });
-    res.render('reports/precious',{admin : "Admin,",logout: "Logout",value,year_system});
-};
+
 exports.top =async (req, res) => {
     const products= await productsModel.list();
     res.render('reports/top',{admin : "Admin,",logout: "Logout",products});
